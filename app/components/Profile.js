6cb189a7236b3ab1844bc91af06c5ba361b02a86
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-08-21 23:52:27 +0000

    test: removido aspas simples

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -12,7 +12,7 @@ export function Profile() {
       <h1>Lorem Ipsum</h1>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book.
       </p>
