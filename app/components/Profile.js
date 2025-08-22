d929aee8cfb4713acc03fa46514b4935d5b73bf2
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-08-22 00:11:38 +0000

    test

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export function Profile() {
   return (
     <div>
       <img
-        src="https://firstbenefits.org/wp-content/uploads/2017/10/placeholder.png"
+        src="https://images.unsplash.com/photo-1755366204962-3cb4bc8452b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="foto de perfil"
         width={128}
         height={128}
