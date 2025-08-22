783078c44ff43c4b478502dc2a8374c40fc9c01c
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-08-22 00:02:00 +0000

    fix

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -10,12 +10,7 @@ export function Profile() {
         height={128}
       />
       <h1>Lorem Ipsum</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
-      </p>
+      <p>Lorem Ipsum.</p>
     </div>
   );
 }
