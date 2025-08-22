eab9c0185f6a077afcd2b84027f4b4d7887e6239
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-08-22 03:04:19 +0000

    feat: projeto finalizado

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,17 +1,24 @@
+import styles from "@/app/page.module.css";
+
 export function Profile() {
   return (
-    <div>
+    <div className={styles.profileCard}>
       <img
-        src="https://images.unsplash.com/photo-1755366204962-3cb4bc8452b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="/images/foto_de_perfil.jpg"
         alt="foto de perfil"
-        width={256}
+        width={128}
+        height={128}
+        className={styles.profilePhoto}
       />
-      <h1>Lorem Ipsum</h1>
+      <h2>Helleson Allan</h2>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry&aposs standard dummy text
-        ever since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
+        Sou um entusiasta da programação apaixonado por resolver problemas e
+        criar soluções inovadoras. Minha jornada começou no ensino médio, onde
+        fiz um curso técnico em programação. Desde então, tenho explorado
+        linguagens como HTML, CSS, JavaScript, Kotlin e TypeScript sempre em
+        busca de desafios que me permitam aprender e crescer como profissional.
+        Estou ansioso para aplicar minhas habilidades em um ambiente
+        profissional e contribuir para o sucesso da equipe.
       </p>
     </div>
   );
