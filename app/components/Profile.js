afd0d2c6b1b919059bb739fc1b079084e7637df5
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-08-22 00:16:53 +0000

    feat: imagem aumentada

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -4,13 +4,13 @@ export function Profile() {
       <img
         src="https://images.unsplash.com/photo-1755366204962-3cb4bc8452b1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="foto de perfil"
-        width={128}
+        width={256}
       />
       <h1>Lorem Ipsum</h1>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry&aposs standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
+        industry. Lorem Ipsum has been the industry&aposs standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book.
       </p>
     </div>
